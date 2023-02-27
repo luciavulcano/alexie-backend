631f080da59f30a63683ca4461381cb92050ebf9
Lúcia Vulcano <vulcanolucia@gmail.com>
2023-02-27 18:37:09 -0300

    Add fields Attr on Meta

--- alexie/serializers.py
+++ alexie/serializers.py
@@ -13,28 +13,34 @@ class MainEmotionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MainEmotion
+        fields = "__all__"
 
 class GeneralEmotionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = GeneralEmotion
+        fields = "__all__"
 
 class GeneralEmotionLogSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = GeneralEmotionLog
+        fields = "__all__"
 
 class HabitSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Habit
+        fields = "__all__"
 
 class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
+        fields = "__all__"
 
 class HealthSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Health
+        fields = "__all__"
