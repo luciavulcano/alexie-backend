082d20929ab1b8215515f399abaffec89d1ae61d
Lucia Vulcano <60199313+luciavulcano@users.noreply.github.com>
2023-05-19 18:07:05 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default="False", cast=bool)
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://alexie-backend-production.up.railway.app']
 
 
 # Application definition
